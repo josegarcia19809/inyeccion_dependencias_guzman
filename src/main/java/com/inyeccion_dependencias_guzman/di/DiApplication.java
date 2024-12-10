5ed739a7f560ae9aa25014314b91373f1ecbf6cd
josegarcia19809 <josegarcia19809@gmail.com>
2024-12-09 23:02:52 -0600

    Inicio de proyecto

---
+++ src/main/java/com/inyeccion_dependencias_guzman/di/DiApplication.java
@@ -0,0 +1,13 @@
+package com.inyeccion_dependencias_guzman.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inyeccion_dependencias_guzman/di/DiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inyeccion_dependencias_guzman.di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
