ac12897004f815c77f411955529e6aadab066818
Jose Garcia <josegarcia19809@gmail.com>
2024-12-27 10:41:21 -0600

    Inyección de dependencias (LOC): Anotación @Primary

---
+++ src/main/java/com/inyeccion_dependencias_guzman/di/repositories/ProductRepositoryFoo.java
@@ -0,0 +1,22 @@
+package com.inyeccion_dependencias_guzman.di.repositories;
+
+import com.inyeccion_dependencias_guzman.di.models.Product;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collections;
+import java.util.List;
+
+@Primary
+@Repository
+public class ProductRepositoryFoo implements ProductRepository {
+    @Override
+    public List<Product> findAll() {
+        return Collections.singletonList(new Product(1L, "Monitor Asus 27", 600L));
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return new Product(1L, "Monitor Asus 27", 600L);
+    }
+}
