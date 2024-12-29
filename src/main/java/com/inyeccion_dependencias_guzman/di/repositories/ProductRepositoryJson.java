694ab307253b6717f7008f8436805d0f54f3b80a
Jose Garcia <josegarcia19809@gmail.com>
2024-12-29 07:43:07 -0600

    Inyección de dependencias (LOC): Creando clase que lee los datos de un archivo json

---
+++ src/main/java/com/inyeccion_dependencias_guzman/di/repositories/ProductRepositoryJson.java
@@ -0,0 +1,36 @@
+package com.inyeccion_dependencias_guzman.di.repositories;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.inyeccion_dependencias_guzman.di.models.Product;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductRepositoryJson implements ProductRepository {
+
+    private List<Product> products;
+
+    public ProductRepositoryJson() {
+        ClassPathResource resource = new ClassPathResource("products.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try{
+            products= Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return products;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return null;
+    }
+}
